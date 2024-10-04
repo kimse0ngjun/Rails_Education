9adbce56df39165ab51f2b0865d61c9bf3c223bf
kimse0ngjun <tjdwns8756@naver.com>
2024-10-04 05:53:38 +0000

    댓글 계시판만들기

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  resources :articles do
+	resources :comments, only: [:create]
+  end
+	
+  # resources :comments
+  # resources :articles
   devise_for :users
   resources :tests
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'tests#index'
+  # root 'tests#index'
+  root 'articles#index'
+	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_02_010717) do
+ActiveRecord::Schema.define(version: 2024_10_04_041729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "comment"
+    t.bigint "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
 
   create_table "tests", force: :cascade do |t|
     t.string "name"
@@ -34,4 +49,5 @@ ActiveRecord::Schema.define(version: 2024_10_02_010717) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "articles"
 end
